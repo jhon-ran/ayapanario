c3e5679daa19333d17ed424ea64779338ef5d56e
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-05-31 15:13:19 -0500

    add sections and index for palabras

---
+++ secciones/palabras/index.php
@@ -0,0 +1,79 @@
+<?php
+//Importar conexión a BD
+include("../../bd.php"); 
+//se inicializa variable de sesión
+session_start();
+
+/*query para insertar palabra con valores de tablas relacionales
+INSERT INTO `palabras` (`id`, `ayapaneco`, `afi`, `id_categoria`, `significado`, `ejemplo_aya`, `ejemplo_esp`, `raiz`, `id_pronunciacion`, `fecha_creacion`) VALUES (NULL, 'ku\'udyi', 'kuʔudʲi', '4', 'aguacate', 'ku\'udyi gwüü', 'aguacate bueno', 'ku\'uti', '1', current_timestamp());
+
+query con inserción con campo pronunciacion vacio
+INSERT INTO `palabras` (`id`, `ayapaneco`, `afi`, `id_categoria`, `significado`, `ejemplo_aya`, `ejemplo_esp`, `raiz`, `id_pronunciacion`, `fecha_creacion`) VALUES (NULL, 'tam', 'tam', '3', 'amargo', 'tam alaxa', 'naranja amarga', 'tam', NULL, current_timestamp());
+*/
+
+/*query para actualizar pronunciacion
+UPDATE `palabras` SET `id_pronunciacion` = '2' WHERE `palabras`.`id` = 1;
+
+para seleccionar pronuncacion de tabla pronunciaciones
+SELECT * FROM `diccionario`.`pronunciaciones` WHERE `id` = 2
+
+para insertar relación de palabra con campo
+INSERT INTO `palabra_campos` (`id`, `id_palabra`, `id_campo`) VALUES (NULL, '1', '5');
+*/
+
+//******Inicia código para mostrar todos los registros******
+//Se prepara sentencia para seleccionar todos los datos 
+$sentencia = $conexion->prepare("SELECT * FROM palabras");
+$sentencia->execute();
+$palabras = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+//Para probar que se esté leyendo todos los datos de la tabla, descomentar
+//print_r($palabras);
+//******Termina código para mostrar todos los registros******
+?>
+<!-- Se llama el header desde los templates-->
+<!-- ../../ sube 2 niveles para poder acceder al folder de templates desde la posición actual-->
+<?php include("../../templates/header.php"); ?>
+
+<h2>Palabras</h2>
+
+<!--Nuevo look inicia-->
+<div class="card">
+  <div class="card-header">
+    <a name="" id="" class="btn btn-primary" href="crear.php" role="button" >Nueva palabra</a>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive-sm">
+      <table class="table" id="tabla_id">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Palabra</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Significado</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach($palabras as $registro){ ?>
+          <tr class="">
+            <td scope="row"><?php echo $registro['id']?></td>
+            <td><?php echo $registro['ayapaneco']?></td>
+            <td><?php echo $registro['id_categoria']?></td>
+            <td><?php echo $registro['significado']?></td>
+            <td>
+              <a name="" id="" class="btn btn-info" href="editar.php?txtID=<?php echo $registro['id']?>" role="button">Editar</a>
+              <!--Se sustituye el link del registro por la función SweatAlert para confirmar borrado-->
+              <a name="" id="" class="btn btn-danger" href="javascript:borrar(<?php echo $registro['id']?>);">Eliminar</a>
+            </td>
+          </tr>
+          <?php }?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+  <!--Nuevo look termina-->
+
+<!-- Se llama el footer desde los templates-->
+<!-- ../../ sube 2 niveles para poder acceder al folder de templates desde la posición actual-->
+<?php include("../../templates/footer.php"); ?>
---
+++ templates/header.php
@@ -0,0 +1,60 @@
+<?php
+//url para conexión en hosting
+//$url_base = "/xpert/";
+//url de base en localhost para concatenar en la navbar y evitar errores de redirección (dinámica)
+//$url_base = "http://localhost/xpert/";
+
+//si no existe la variable de sesión usuario_id, se redirige al login
+/*
+if(!isset($_SESSION['usuario_id'])){
+    header('Location:'.$url_base.'login.php');
+    exit();
+}
+*/
+
+//print_r($_SESSION['usuario_tipo'])
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <!-- estilo para personalizar -->
+        <!--<link rel="stylesheet" href="../style.css">-->
+        <!-- cdn JQuery v.3.7.1-->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <!-- cdn DataTables v.1.12.1 -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.dataTables.css" />
+        <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
+        <!-- cdn para Sweet Alert 2, alertas de acciones -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+
+    <body>
+        <header>
+            
+        </header>       
+
+        <main class="container">
+            <!--Inicia código de mensaje de alerta cuando se borra registro-->
+        <!--Si hay algo en el métod get-->
+        <?php if(isset($_GET['mensaje'])){ ?>
+            <!--se corre el mensaje de eliminado en línea 19-->
+            <script>
+                Swal.fire({icon:"success", title:"<?php echo $_GET['mensaje'];?>"});
+            </script>
+        <?php } ?>
+        <!--Termina código de mensaje de alerta cuando se borra registro-->
